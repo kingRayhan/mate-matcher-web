730d9fe4f5efd9724b8f62e876d51eeb49fdd3b0
KingRayhan <rayhan095@gmail.com>
2023-03-23 23:59:26 +0600

    posts

--- src/apis/posts/service.ts
+++ src/apis/posts/service.ts
@@ -6,6 +6,26 @@ export class PostService {
       title: "Post 1",
       content: "Post 1 content",
     },
+    {
+      id: 2,
+      title: "Post 2",
+      content: "Post 2 content",
+    },
+    {
+      id: 3,
+      title: "Post 3",
+      content: "Post 3 content",
+    },
+    {
+      id: 4,
+      title: "Post 4",
+      content: "Post 4 content",
+    },
+    {
+      id: 5,
+      title: "Post 5",
+      content: "Post 5 content",
+    },
   ];
 
   getPosts() {
